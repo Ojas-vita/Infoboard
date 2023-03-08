e8b940aac302e26d32a3effc2e040b12ea4cf02e
Ojas-vita <ojasvitasharma.dcmc@gmail.com>
2023-03-08 21:06:06 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+import 'default.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        textTheme: TextTheme(
+          headline1: TextStyle(
+              fontSize: 49.0,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Roboto'),
+          headline2: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Roboto'),
+          bodyText1: TextStyle(
+              fontSize: 17.0,
+              fontWeight: FontWeight.w400,
+              fontFamily: 'Roboto'),
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+      initialRoute: "default",
+      routes: {
+        "default": (context) => Default(),
+        // "medicine": (context) => Medicine(),
+        // "Lab_Report": (context) => LabReport(),
+      },
+    );
+  }
+}
